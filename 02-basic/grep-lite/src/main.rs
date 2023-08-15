bc43277a3bb45f3eda1dd76bfc177a10aa4a6dbc
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2023-08-15 18:20:12 +0900

    2.13

--- 02-basic/grep-lite/src/main.rs
+++ 02-basic/grep-lite/src/main.rs
@@ -1,5 +1,8 @@
 use clap::{App, Arg};
 use regex::Regex;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 fn main() {
     let args = App::new("grep-lite")
@@ -13,15 +16,15 @@ fn main() {
         )
         .get_matches();
     let pattern = args.value_of("pattern").unwrap();
-    let re = Regex::new("picture").unwrap();
-    let quote = "
-    Every face, every shop, bedroom window, public-house, and
-    dark square is a picture feverishly turned--in search of what?
-    It is the same with books. What do we seek through millions of pages?";
+    let re = Regex::new(pattern).unwrap();
 
-    for line in quote.lines() {
-        let contains_substring = re.find(line);
-        match contains_substring {
+    let input = args.value_of("input").unwrap();
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        match re.find(&line) {
             Some(_) => println!("{}", line),
             None => (),
         }
