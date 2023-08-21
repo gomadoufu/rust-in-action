eb6c2c31be202c1604d4f272764c0269fdf4fce4
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2023-08-22 03:31:46 +0900

    3.9

---
+++ 03-composite-types/doc/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"implementing_display":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"implementing_display/enum.FileState.html\" title=\"enum implementing_display::FileState\">FileState</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"implementing_display/struct.File.html\" title=\"struct implementing_display::File\">File</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
